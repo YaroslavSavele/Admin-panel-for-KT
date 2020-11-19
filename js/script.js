dba2ef18bef76eead14b4523f6587625ffe7d091
YaroslavSavele <asis174@mail.ru>
2020-11-19 14:11:33 +0500

    make admin panel

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.aside-nav');
+const buttonClose = document.querySelector('.close');
+const activePage= document.querySelector('.active');
+
+
+
+burger.addEventListener('click', () => {
+   menu.classList.add('is-open');
+});
+
+const close = function () {
+   menu.classList.remove('is-open');
+};
+
+buttonClose.addEventListener('click', () => {
+   close();
+});
+
+
+   activePage.addEventListener('click', (e) => {
+      e.preventDefault();
+      close();
+   });
