1164ef6104ba944529b2ca2b49c9c12a5ccd277b
YaroslavSavele <asis174@mail.ru>
2020-11-25 16:11:19 +0500

    make popup

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.aside-nav');
 const buttonClose = document.querySelector('.close');
-const activePage= document.querySelector('.active');
+const activePage = document.querySelector('.active');
 
 
 
@@ -19,22 +19,41 @@ buttonClose.addEventListener('click', () => {
 });
 
 
-   activePage.addEventListener('click', (e) => {
-      e.preventDefault();
-      close();
-   });
+activePage.addEventListener('click', (e) => {
+   e.preventDefault();
+   close();
+});
 
-   //Checkbox script
-   const checkboxes = document.querySelectorAll('.checkbox');
+//Checkbox script
+const checkboxes = document.querySelectorAll('.checkbox');
+let isBuble = false;
 
-   for (let chek of checkboxes) {
-      chek.addEventListener('change', () => {
-         let address = chek.previousElementSibling;
-         address.classList.toggle('bright');
-      });
-      let inp = chek.querySelector('input');
+for (let chek of checkboxes) {
+   chek.addEventListener('change', () => {
       let address = chek.previousElementSibling;
-      if (inp.checked) {
-         address.classList.add('bright');
-      }
+      address.classList.toggle('bright');
+      isBuble = true;
+   });
+   let inp = chek.querySelector('input');
+   let address = chek.previousElementSibling;
+   if (inp.checked) {
+      address.classList.add('bright');
    }
+}
+
+// Popup script
+
+const hit = document.querySelector('.popup--success');
+const mistake = document.querySelector('.popup--error');
+
+if (response.ok) {
+   console.log(isBuble);
+   hit.classList.add('emerge');
+   setTimeout(() => { hit.classList.remove('emerge') }, 2000);
+} else {
+   mistake.classList.add('emerge');
+   setTimeout(() => { mistake.classList.remove('emerge') }, 2000);
+};
+
+
+
